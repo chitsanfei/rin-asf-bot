671446b08ae8c9ec14db8574430040d7d90f48a0
MashiroSA <emu-ws@outlook.com>
2023-11-18 23:55:32 +0800

    fix: try-1

--- rin-asf-bot/Api/CatAPI.cs
+++ rin-asf-bot/Api/CatAPI.cs
@@ -4,6 +4,9 @@ using ArchiSteamFarm.Web;
 using ArchiSteamFarm.Web.Responses;
 using System.Diagnostics.CodeAnalysis;
 using Newtonsoft.Json;
+using System.Threading;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace ArchiSteamFarm.CustomPlugins.Rin.Api;
 
@@ -11,22 +14,20 @@ internal static class CatAPI
 {
 	private const string URL = "https://api.thecatapi.com";
 
-	internal static async Task<Uri?> GetRandomCatURL(WebBrowser webBrowser)
-	{
+	internal static async Task<Uri?> GetRandomCatURL(WebBrowser webBrowser, CancellationToken cancellationToken = default) {
 		ArgumentNullException.ThrowIfNull(webBrowser);
 
 		Uri request = new($"{URL}/v1/images/search");
 
-		ObjectResponse<MeowResponse>? response = await webBrowser.UrlGetToJsonObject<MeowResponse>(request).ConfigureAwait(false);
+		ObjectResponse<ImmutableList<MeowResponse>>? response = await webBrowser.UrlGetToJsonObject<ImmutableList<MeowResponse>>(request, cancellationToken: cancellationToken).ConfigureAwait(false);
 
-		return response?.Content?.URL;
+		return response?.Content?.FirstOrDefault()?.URL;
 	}
 }
 
 [SuppressMessage("ReSharper", "ClassCannotBeInstantiated")]
-internal sealed class MeowResponse
-{
-	[JsonProperty("file", Required = Required.Always)]
+internal sealed class MeowResponse {
+	[JsonProperty("url", Required = Required.Always)]
 	internal readonly Uri URL = null!;
 
 	[JsonConstructor]
